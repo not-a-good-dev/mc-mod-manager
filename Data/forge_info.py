e7aa14eb003aecb9117e573c2aad8fa524fd375f
no-u <81192508+no-u101@users.noreply.github.com>
2021-11-01 19:41:07 -0700

    added files

---
+++ Data/forge_info.py
@@ -0,0 +1,13 @@
+
+
+name = ("forge")
+author = ("no u")
+version = ("0.0.1")
+mod_id = ("forge")
+IFversion = ("alpha")
+packageVersion = ("1.0.0")
+description = ("-do not use- multi version")
+
+
+class downloads():
+    e=()
